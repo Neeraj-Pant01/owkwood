907eb5fcc83c45ab3797636a221e334b1cd705b2
Neeraj-Pant01 <npant923@gmail.com>
2024-02-11 00:45:26 +0530

    change2

--- src/App.jsx
+++ src/App.jsx
@@ -2,27 +2,13 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 
 
 import React from 'react'
-import NavigationBar from './components/NavigationBar'
-import CarouselComponent from './components/CarousalComponent';
-import About from './components/About';
-import OurDeals from './components/OurDeals';
-import LuxuryLiving from './components/LuxuryLiving';
-import Services from './components/Services';
-import Testimonial from './components/Testimonials';
-import Team from './components/Team';
+import Homepage from './pages/Homepage';
 
 
 const App = () => {
   return (
     <>
-    <NavigationBar />
-    <CarouselComponent />
-    <About />
-    <OurDeals />
-    <LuxuryLiving />
-    <Services />
-    <Testimonial />
-    <Team />
+    <Homepage />
     </>
   )
 }
---
+++ src/pages/Homepage.jsx
@@ -0,0 +1,26 @@
+import React from 'react'
+import NavigationBar from '../components/NavigationBar'
+import CarouselComponent from '../components/CarousalComponent'
+import About from '../components/About'
+import OurDeals from '../components/OurDeals'
+import LuxuryLiving from '../components/LuxuryLiving'
+import Services from '../components/Services'
+import Testimonial from '../components/Testimonials'
+import Team from '../components/Team'
+
+const Homepage = () => {
+  return (
+    <>
+    <NavigationBar />
+    <CarouselComponent />
+    <About />
+    <OurDeals />
+    <LuxuryLiving />
+    <Services />
+    <Testimonial />
+    <Team />
+    </>
+  )
+}
+
+export default Homepage
